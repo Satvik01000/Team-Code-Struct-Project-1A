7d778127789eecaba0921dc2348aefa5e3a2d05e
Satvik Mishra <satvikmishra2192@gmail.com>
2025-07-16 23:33:05 +0530

    Added Feature to extract text data from PDF's

---
+++ Challenge 1A/app/extract_features.py
@@ -0,0 +1,22 @@
+import fitz
+
+def extract_spans(pdf_path):
+    doc = fitz.open(pdf_path)
+    all_spans = []
+
+    for page_num, page in enumerate(doc):
+        blocks = page.get_text("dict")["blocks"]
+        for block in blocks:
+            for line in block.get("lines", []):
+                text = " ".join(span["text"].strip() for span in line["spans"])
+                if not text:
+                    continue
+                span = line["spans"][0]
+                all_spans.append({
+                    "text": text,
+                    "font": span["font"],
+                    "size": span["size"],
+                    "top": span["bbox"][1],
+                    "page": page_num + 1
+                })
+    return all_spans
